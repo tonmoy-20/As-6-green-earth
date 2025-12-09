074963db3b4a860f9d63995efce1d39f42fc69e3
Tonmoy <tonmoybiswas20602@gmail.com>
2025-12-09 23:49:25 +0600

    load category added

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ const loadCategory = () => {
     .then((res) => res.json())
     .then((data) => displayCategory(data.categories));
 };
+let cart = [];
+let total = 0;
+let cnt = 1;
+
+const randomTreeLoad = () => {
+  const url = "https://openapi.programming-hero.com/api/plants";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayTrees(data.plants));
+};
+
+const loadTrees = (id) => {
+  const url = id
+    ? `https://openapi.programming-hero.com/api/category/${id}`
+    : `https://openapi.programming-hero.com/api/plants`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayTrees(data.plants));
+};
